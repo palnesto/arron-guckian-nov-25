c26e4c6003c338bd80a8843f6915097df4d2481f
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2025-11-12 19:07:19 +0530

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,44 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import Pages from "vite-plugin-pages";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-  },
-  plugins: [
-    react(),
-    Pages(),
-    ViteImageOptimizer({
-      png: {
-        quality: 80,
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  return {
+    plugins: [
+      react(),
+      Pages(),
+      ViteImageOptimizer({
+        png: {
+          quality: 80,
+        },
+        jpg: {
+          quality: 80,
+        },
+        jpeg: {
+          quality: 80,
+        },
+        gif: {},
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-      jpg: {
-        quality: 80,
-      },
-      jpeg: {
-        quality: 80,
-      },
-      gif: {},
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
+    build: {
+      outDir: "dist", // or set to "/app/dist"
+    },
+    server:
+      env.VITE_MODE === "development" || env.VITE_MODE === "local"
+        ? {
+            proxy: {
+              "/api": env.VITE_BACKEND_URL,
+            },
+            port: 5173,
+          }
+        : undefined,
+  };
 });
